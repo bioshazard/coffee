c12e124d9481c43626a52e68dc27d6b12e8f2fd4
Joe Still <joe@bioshazard.com>
2025-05-17 15:31:26 -0500

    fixed local dev env

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/"
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // a bunch of stuff to get code server working
+    base: mode === 'development' ? "/absproxy/5173" : "/",
+    server: mode === 'development' ? {
+      allowedHosts: [
+        "bios-kubuntu.home.arpa",
+        "code.lab1.bios.dev",
+      ],
+      proxy: {
+        '/api': {
+          target: 'http://172.18.0.1:54321',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    } : undefined,
+  }
 })
