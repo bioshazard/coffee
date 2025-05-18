39bcddb1c2d8cc9f3ca8c31bde9d0ff67663111c
Joe <bioshazard@users.noreply.github.com>
2025-05-17 20:14:32 -0500

    Disable non-To-Do vote restriction

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -493,7 +493,10 @@ export default function Board(props) {
   // Loop through each card that has votes
   // If any voted card is past first column, voting is disabled.
   // const votingDisabled = Object.keys(voteTotals).some( cardId => cards.some( card => ) )
-  const votingDisabled = cards.some( card => card.col != 0 && voteTotals.calculated[card.id])
+  // Voting was previously disabled once any vote was present in a non-To-Do column.
+  // This logic has been turned off by forcing votingDisabled to false.
+  // const votingDisabled = cards.some( card => card.col != 0 && voteTotals.calculated[card.id])
+  const votingDisabled = false
   //   console.log(card, card.col, card.col != 0, card.id, voteTotals, voteTotals.calculated[card.id])
 
   // } )
